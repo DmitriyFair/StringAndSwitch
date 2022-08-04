0b17b15fd77dcb93bd1f1af5230526add7ed233d
DmitriyFair <dmitrofair@gmail.com>
2022-08-05 00:48:32 +0300

    Change1

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ public class Main {
     // При выведении месяца указывется время года (либо Такой месяц не существует). С помощью if else.
     public static void main(String[] args) {
         String month  = "";
-        if (month == "January" || month == "February" || month == "December"){
+        if (month.equals("January") || month.equals("February") || month.equals("December")) {
             System.out.println("The winter");
         }
-        else if (month == "March" || month == "April" || month == "May"){
+        else if (month.equals("March") || month.equals("April") || month.equals("May")){
             System.out.println("The spring");
         }
-        else if(month == "June" || month == "July" || month == "August"){
+        else if(month.equals("June") || month.equals("July") || month.equals("August")){
             System.out.println("The summer");
         }
-        else if(month == "September" || month == "October" || month == "November"){
+        else if(month.equals("September") || month.equals("October") || month.equals("November")){
             System.out.println("The fall");
         }
         else System.out.println("This month does not exist");
--- src/Main1.java
+++ src/Main1.java
@@ -4,43 +4,28 @@ public class Main1 {
         String month = "";
         switch (month){
             case "January":
-                System.out.println("The winter");
-                break;
             case "February":
-                System.out.println("The winter");
+            case "December":
+                System.out.println("The spring");
                 break;
             case "March":
-                System.out.println("The spring");
-                break;
             case "April":
-                System.out.println("The spring");
-                break;
             case "May":
-                System.out.println("The spring");
+                System.out.println("The summer");
                 break;
             case "June":
-                System.out.println("The summer");
-                break;
             case "July":
-                System.out.println("The summer");
-                break;
             case "August":
-                System.out.println("The summer");
+                System.out.println("The fall");
                 break;
             case "September":
-                System.out.println("The fall");
-                break;
             case "October":
-                System.out.println("The fall");
-                break;
             case "November":
-                System.out.println("The fall");
-                break;
-            case "December":
                 System.out.println("The winter");
                 break;
             default:
                 System.out.println("This month does not exist");
+                break;
         }
     }
 }
